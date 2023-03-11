04acf39f105df8ab96059a5e94f268c28eadb234
triet <pvmtriet232@gmail.com>
2023-03-12 00:32:47 +0700

    refactory code

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,42 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"strconv"
 
 	"net/http"
 )
-
-func home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	w.Write([]byte("Hello from Snippetbox2"))
-}
-
-func showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil {
-		http.NotFound(w, r)
-		return
-	}
-
-	fmt.Fprintf(w, "The id of specific snippet is %d...", id)
-}
-
-func createSnippet(w http.ResponseWriter, r *http.Request) {
-
-	if r.Method != http.MethodPost {
-		// use Header().set() to add 'Allow: POST' header ...
-		w.Header().Set("Allow", http.MethodPost)
-		// use http.Error() to shortcut w.WriteHeader() and w.Write()
-		http.Error(w, "Method Not Allowed", 405)
-		return
-	}
-	w.Write([]byte("Create a new snippet..."))
-}
 
 func main() {
 	mux := http.NewServeMux()
