71f36f5ce7829bc45106e68cd883aba365249595
triet <pvmtriet232@gmail.com>
2023-03-11 19:38:22 +0700

    restrict the root URL pattern

--- main.go
+++ main.go
@@ -7,12 +7,26 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific Snippet"))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create s new snippet"))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
