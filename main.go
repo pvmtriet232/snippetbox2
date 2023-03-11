1854478769573eb18c609581d7d9c03fbf42ea8b
triet <pvmtriet232@gmail.com>
2023-03-11 18:44:02 +0700

    add dockerfile installing air

--- main.go
+++ main.go
@@ -9,10 +9,19 @@ import (
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
+func create(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create here"))
+}
+
+func pvmtriet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Devops king"))
+}
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/create", create)
+	mux.HandleFunc("/create/pvmtriet", pvmtriet)
 
 	log.Println("starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
