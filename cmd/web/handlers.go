cec34be31fa5ac0fc200dbde96818d7dc993c287
triet <pvmtriet232@gmail.com>
2023-03-12 22:54:29 +0700

    parse both template files

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -14,12 +14,12 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Use the template.ParseFiles() function to read the template file into a
-	// template set. If there's an error, we log the detailed error message and use
-	// the http.Error() function to send a generic 500 Internal Server Error
-	// response to the user.
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+	}
 
-	ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
+	ts, err := template.ParseFiles(files...)
 
 	if err != nil {
 		log.Println(err.Error())
