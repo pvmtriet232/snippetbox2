5f3a046c1857eca64139f876071691b9fa4635bf
triet <pvmtriet232@gmail.com>
2023-03-14 03:21:09 +0700

    change dsn

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -22,7 +22,7 @@ func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	// Define a new command-line flag for the MySQL DSN string.
-	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
+	dsn := flag.String("dsn", "web:pass@tcp(127.0.0.1:3306)/snippetbox", "MySQL data source name")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
