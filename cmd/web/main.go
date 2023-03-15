790197f5f8dcb0084e58d9c4b30a3ef175d92565
triet <pvmtriet232@gmail.com>
2023-03-16 02:23:21 +0700

    add parseTime

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -25,7 +25,7 @@ func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	// Define a new command-line flag for the MySQL DSN string.
-	dsn := flag.String("dsn", "web:pass@tcp(mysql-snippet:3306)/snippetbox", "MySQL data source name")
+	dsn := flag.String("dsn", "web:pass@tcp(mysql-snippet:3306)/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
