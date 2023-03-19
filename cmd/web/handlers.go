37b8263d3886b3db368882d6ebbbec89f5f3d6dc
triet <pvmtriet232@gmail.com>
2023-03-19 10:34:03 +0700

    re-display errors and form data passed to template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -51,50 +51,38 @@ func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request
 	app.render(w, r, "create.page.tmpl", nil)
 }
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-	// First we call r.ParseForm() which adds any data in POST request bodies
-	// to the r.PostForm map. This also works in the same way for PUT and PATCH
-	// requests. If there are any errors, we use our app.ClientError helper to send
-	// a 400 Bad Request response to the user.
 	err := r.ParseForm()
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
-	// Use the r.PostForm.Get() method to retrieve the relevant data fields
-	// from the r.PostForm map.
 	title := r.PostForm.Get("title")
 	content := r.PostForm.Get("content")
 	expires := r.PostForm.Get("expires")
-
-	// Initialize a map to hold any validation errors.
 	errors := make(map[string]string)
-
-	// long. If it fails either of those checks, add a message to the errors
-	// map using the field name as the key.
 	if strings.TrimSpace(title) == "" {
 		errors["title"] = "This field cannot be blank"
 	} else if utf8.RuneCountInString(title) > 100 {
 		errors["title"] = "This field is too long (maximum is 100 characters)"
 	}
-	// Check that the Content field isn't blank.
 	if strings.TrimSpace(content) == "" {
 		errors["content"] = "This field cannot be blank"
 	}
-	// Check the expires field isn't blank and matches one of the permitted
-	// values ("1", "7" or "365").
 	if strings.TrimSpace(expires) == "" {
 		errors["expires"] = "This field cannot be blank"
 	} else if expires != "365" && expires != "7" && expires != "1" {
 		errors["expires"] = "This field is invalid"
 	}
-	// If there are any errors, dump them in a plain text HTTP response and return
-	// from the handler.
+	// If there are any validation errors, re-display the create.page.tmpl
+	// template passing in the validation errors and previously submitted
+	// r.PostForm data.
 	if len(errors) > 0 {
-		fmt.Fprint(w, errors)
+		app.render(w, r, "create.page.tmpl", &templateData{
+			FormErrors: errors,
+			FormData:   r.PostForm,
+		})
 		return
 	}
-
-	// Create a new snippet record in the database using the form data.
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
 		app.serverError(w, err)
